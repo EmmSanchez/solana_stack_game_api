bc23ac06631e3223d40dd7c85156e526afe1fe9b
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-11-12 20:14:01 -0600

    Added new railway URL

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.disable("x-powered-by");
 const ACCEPTED_ORIGINS = [
   "http://localhost:5173",
   "https://solana-stack-game.vercel.app",
+  "https://solanastackgameapi-production-a634.up.railway.app/",
   "http://192.168.3.44:5173",
 ];
 
